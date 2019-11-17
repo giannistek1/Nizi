16e195496504e5943767c3970ce47a5528e1193a
Gianni <gianni.stekkinger@hotmail.nl>
2019-11-17 22:34:53 +0100

    Nizi app

---
+++ app/src/main/java/nl/stekkinger/nizi/MainActivity.kt
@@ -0,0 +1,12 @@
+package nl.stekkinger.nizi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
