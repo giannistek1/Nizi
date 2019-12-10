b2a4b3499e791d1319fb60a2621834bb21ed6dce
rene-steneker <rene.steneker@gmail.com>
2019-12-10 19:56:02 +0100

    accesToken works by sharedpref from repo, added application context

--- app/src/main/java/nl/stekkinger/nizi/MainActivity.kt
+++ app/src/main/java/nl/stekkinger/nizi/MainActivity.kt
@@ -1,5 +1,6 @@
 package nl.stekkinger.nizi
 
+import android.content.Context
 import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
@@ -47,6 +48,8 @@ class MainActivity : AppCompatActivity() {
 
         // Obtain the token from the Intent's extras
         accessToken = intent.getStringExtra(EXTRA_ACCESS_TOKEN)
+        val preferences = getSharedPreferences("NIZI", Context.MODE_PRIVATE)
+        preferences.edit().putString("TOKEN", accessToken).apply()
 
         // Login
         loginPatientAsyncTask(accessToken)
--- app/src/main/java/nl/stekkinger/nizi/repositories/FoodRepository.kt
+++ app/src/main/java/nl/stekkinger/nizi/repositories/FoodRepository.kt
@@ -1,10 +1,15 @@
 package nl.stekkinger.nizi.repositories
 
+import android.app.Application
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.preference.PreferenceManager
 import android.util.Log
 import android.util.Log.d
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import nl.stekkinger.nizi.ApiService
+import nl.stekkinger.nizi.NiziApplication
 import nl.stekkinger.nizi.classes.*
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -23,11 +28,14 @@ class FoodRepository {
     val grant_type = "client_credentials"
     val test_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5ERkdPRFUxTnpJNFJEZ3lNakkxUmtFMU5EZ3dRMEUxTkVJM05UTTBSRGRFUTBFNE5FWkdNZyJ9.eyJpc3MiOiJodHRwczovL2FwcG5pemkuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVkZGZiM2I5MjlmOWNkMGUzMjA0MDViZiIsImF1ZCI6WyJhcHBuaXppLm5sL2FwaSIsImh0dHBzOi8vYXBwbml6aS5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNTc1OTY2ODk0LCJleHAiOjE1NzYwNTMyOTQsImF6cCI6InU5MzdvZncwQllQR1VIUGdOVXJmQnBzcElhMFZTQzg2Iiwic2NvcGUiOiJvcGVuaWQifQ.Vd3PdGhs0slRbkhbHkHwB_BM-Ch5Tij497cZJqM29FpOLI8DZMNpG0yeJSS2PJXRWrst8kcHf7VhZvvC22kttRCcGiDum-_1wBvfXT4sB0k5xMh75hRufV5doN47cLR6wXAnt7OzjI5u5huoxcG1afKo1hCkrHOz7v-liSUGqXbbj5sKAA0Krm640_eQKY4bjyU2VkOduZbgyDo0DZ_tzJ9xHutPxhEDA_TkTmyRgSeRey5di3ZUunf0-nw1lN6Xs7lMYHowPe39-u0EIW8f90aY8kmsVC2LM3kUr9w7w0gzd3ebXK00IJhwlwcTMMAXmWZeNUACPg144VS1IfXZCQ"
 
+    var preferences = NiziApplication.instance.getSharedPreferences("NIZI", Context.MODE_PRIVATE)
+    var accessToken = preferences.getString("TOKEN", null)
+
     fun searchFood(search: String): MutableLiveData<ArrayList<Food>?> {
         var foodList: ArrayList<Food> = arrayListOf()
         val result = MutableLiveData<ArrayList<Food>?>()
 
-        var authHeader = "Bearer " + test_token
+        var authHeader = "Bearer " + accessToken
 
         service.searchFoodDB(authHeader = authHeader, foodName = "b", count = 5).enqueue(object: Callback<ArrayList<Food>> {
             override fun onResponse(call: Call<ArrayList<Food>>, response: Response<ArrayList<Food>>) {
