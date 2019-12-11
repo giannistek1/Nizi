dd73ecd80f1c95a41f46fc5e5a60d4f80b0642d4
Gianni <gianni.stekkinger@hotmail.nl>
2019-12-11 10:39:36 +0100

    Merge fixes

--- app/src/main/java/nl/stekkinger/nizi/fragments/FoodViewFragment.kt
+++ app/src/main/java/nl/stekkinger/nizi/fragments/FoodViewFragment.kt
@@ -1,23 +1,28 @@
 package nl.stekkinger.nizi.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import nl.stekkinger.nizi.classes.DiaryViewModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_food_view.*
 import nl.stekkinger.nizi.R
+import nl.stekkinger.nizi.classes.DiaryViewModel
 import nl.stekkinger.nizi.classes.Food
 
 class FoodViewFragment : Fragment() {
 
     private lateinit var model: DiaryViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view: View = inflater.inflate(R.layout.fragment_food_view, container, false)
-        //TODO: add food item data
+        setHasOptionsMenu(true)
 
         // get the DiaryViewModel
         model = activity?.run {
@@ -26,9 +31,32 @@ class FoodViewFragment : Fragment() {
 
         model.selected.observe(this, Observer<Food> { food ->
             // Update the UI
+            title_food_view.text = food.Name
+            Picasso.get().load(food.Picture).into(image_food_view)
+            serving_size_value.text = food.PortionSize.toString() + " " + food.WeightUnit
+            calories_value_food_view.text = food.KCal.toString() + " Kcal"
+            fiber_value_food_view.text = food.Fiber.toString() + " g"
+            protein_value_food_view.text = food.Protein.toString() + " g"
+            water_value_food_view.text = "10 g" // TODO update API with water input
+            sodium_value_food_view.text = (food.Sodium * 1000).toString() + " mg"
+            calcium_value_food_view.text = (food.Calcium * 1000).toString() + " mg"
 
         })
 
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.menu_confirm, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.confirm_add_food -> {
+                Toast.makeText(this.context, "adding", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
