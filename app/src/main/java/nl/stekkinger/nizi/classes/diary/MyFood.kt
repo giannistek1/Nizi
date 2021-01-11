87647c2fc2bca77cbc7e25f9322f65d94bbd541b
Gianni <gianni.stekkinger@hotmail.nl>
2021-01-11 20:48:31 +0100

    Fixed EditPatient

--- app/src/main/java/nl/stekkinger/nizi/classes/diary/MyFood.kt
+++ app/src/main/java/nl/stekkinger/nizi/classes/diary/MyFood.kt
@@ -1,8 +1,10 @@
 package nl.stekkinger.nizi.classes.diary
 
+import java.io.Serializable
+
 class MyFood(
     val id: Int,
     val food: Int,
     val created_at: String? = null,
     val updated_at: String? = null
-)
+) : Serializable
